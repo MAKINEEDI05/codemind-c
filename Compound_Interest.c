f6eeb07f482a0df7fa043fcef02a17329f7dda96
MAKINEEDI LAKSHMI NARAYANA <142885683+MAKINEEDI05@users.noreply.github.com>
2023-12-25 09:35:58 +0530

    // compound interest #include <stdio.h> #include <math.h> int main() {     float p,r,t;     float ci;     scanf("%f%f%f",&p,&r,&t);     ci=(p*pow(1+(r/100),t))-p;     printf("%.2f",ci); }

---
+++ Compound_Interest.c
@@ -0,0 +1,11 @@
+// compound interest
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float p,r,t;
+    float ci;
+    scanf("%f%f%f",&p,&r,&t);
+    ci=(p*pow(1+(r/100),t))-p;
+    printf("%.2f",ci);
+}
